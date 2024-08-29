f0b3e07d5193293d99e5376cf489d373838e078c
joelocano <joelangelocano@gmail.com>
2024-08-28 21:54:35 -0500

    feat: added font family readex pro and text body color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const daisyui = require("daisyui");
+const typography = require("@tailwindcss/typography");
+const tailwind_theme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Readex Pro", ...tailwind_theme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [require("daisyui")],
+  plugins: [typography, daisyui],
   daisyui: {
-    themes: ["cupcake", "dim", "cmyk"], // false: only light + dark | true: all
+    themes: ["cupcake", "dim", "cmyk"],
   },
 };
